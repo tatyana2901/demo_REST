66994185447fcba565a7b07a0997310d6cccd7fc
tatiana.anisimova <tznatkova90@gmail.com>
2024-12-21 15:22:14 +0300

    Initial commit

---
+++ src/main/java/com/REST_API/demo/EmployeeDao.java
@@ -0,0 +1,7 @@
+package com.REST_API.demo;
+
+import java.util.List;
+
+public interface EmployeeDao {
+    public List<Employee> getAllEmployees();
+}
---
+++ src/test/java/com/REST_API/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.REST_API.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
